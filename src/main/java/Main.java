60066c6a4c607f7dd5cda894fa6e60820e3a5b8f
WillianPaiva <vervalenpaiva@gmail.com>
2016-12-28 17:48:05 +0100

    kn-means

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +19,9 @@ import org.apache.log4j.Logger;
 import static java.lang.System.exit;
 
 /**
- * ARGS: 0: output file, 1: input file, 2: k-means, 3: columns, 4: separator between values
- * @author alaguitard
+ * ARGS: 0: input file, 1: output file, 2: k-means, 3: n-colunms
  */
+
 public class Main {
   private static Logger logger = Logger.getLogger(Main.class);
 	public static String KVALUES = "/values.txt";
@@ -29,7 +30,8 @@ public class Main {
 	public static List<Double> mCenters = new ArrayList<Double>();
 
     public static void main(String[] args) throws Exception {
-      if(args.length < 5)
+      ArrayList<ArrayList<Double>> res = new ArrayList<ArrayList<Double>>();
+      if(args.length < 4)
       {
         System.err.println("Illegal number of argument");
         exit(0);
@@ -43,18 +45,59 @@ public class Main {
       }
 
       if(fs.exists(new Path(KVALUES))){
-        /*If exist delete the output path*/
+        /*If exist delete the  tmp output path*/
         fs.delete(new Path(KVALUES),true);
       }
       conf.set("k",args[2]);
-      conf.set("column",args[3]);
+
+      // makes a k means for each colunm passed on the argument 
+      for(int i = 3; i<args.length ; i++){
+        res.add(calculate(args,conf,args[i],fs));
+        //redelete the tmp folder to avoid error in case of a new calculation
+        if(fs.exists(new Path(KVALUES))){
+          /*If exist delete the  tmp output path*/
+          fs.delete(new Path(KVALUES),true);
+        }
+      }
+
+
+
+      //print the array list to chck the result
+      int colunmres = 1;
+      for(ArrayList<Double> l:res){
+        int kl = 1;
+        for(Double d:l){
+          logger.info("=========== colunm "+colunmres+" ============= k "+kl+" = "+d);
+          kl++;
+        }
+        colunmres++;
+      }
+    }
+
+
+  public static ArrayList<Double> calculate(String[] args,
+                                      Configuration conf,
+                                      String colunm,
+                                      FileSystem fs)
+      throws IllegalArgumentException,
+      IOException,
+      ClassNotFoundException,
+      InterruptedException{
+
+    conf.set("column",colunm);
       int iter = 0;
       boolean finished = false;
+      ArrayList<Double> result = new ArrayList<Double>();
       while(!finished){
+        /////////////////////////////////////
+        // iterations loop for 1 dimension //
+        /////////////////////////////////////
         if(iter == 0){
+          ///////////////////////////////////////////////////
+          // first iteration needs to create the base file //
+          ///////////////////////////////////////////////////
           Path input = new Path(args[0]);
           getValues(conf, input, Integer.parseInt(args[2]), Integer.parseInt(args[3]));
-
           Job job = Job.getInstance(conf, "Main");
           job.setNumReduceTasks(1);
           job.setJarByClass(Main.class);
@@ -70,33 +113,36 @@ public class Main {
           FileOutputFormat.setOutputPath(job, new Path(KVALUES));
           job.waitForCompletion(true);
           iter++;
-
         }else{
+          /////////////////////////////
+          // not the first iteration //
+          /////////////////////////////
+
+          //read the k-means from last iteration
           Path input = new Path(KVALUES+"/part-r-00000");
 
           int k = Integer.parseInt(args[2]);
-          ArrayList<Double> centers= new ArrayList<Double>();
 
+          ArrayList<Double> centers= new ArrayList<Double>();
           for(int i=0;i<k;i++)
           {
            double kw = Double.parseDouble(conf.get("k"+i));
             centers.add(kw);
           }
-
           getValues(conf, input, k, 0);
-
+          //check if the k values are the same from last iteration 
           finished = true;
           for(int i=0;i<k;i++)
           {
-
             double a = Double.parseDouble(conf.get("k"+i));
             double b = centers.get(i);
-            logger.info(a+"==============="+b);
             if(a != b){
               finished = false;
             }
           }
           if(!finished){
+            //in case the not finish keep the iterations
+
             if(fs.exists(new Path(KVALUES))){
               /*If exist delete the output path*/
               fs.delete(new Path(KVALUES),true);
@@ -116,39 +162,31 @@ public class Main {
             FileOutputFormat.setOutputPath(job, new Path(KVALUES));
             job.waitForCompletion(true);
             iter++;
+
           }else{
-            Job job = Job.getInstance(conf, "Main");
-            job.setNumReduceTasks(1);
-            job.setJarByClass(Main.class);
-            job.setMapperClass(KMeansMapper.class);
-            job.setMapOutputKeyClass(DoubleWritable.class);
-            job.setMapOutputValueClass(DoubleWritable.class);
-            job.setReducerClass(KMeansReducer.class);
-            job.setOutputKeyClass(DoubleWritable.class);
-            job.setOutputValueClass(DoubleWritable.class);
-            job.setOutputFormatClass(TextOutputFormat.class);
-            job.setInputFormatClass(TextInputFormat.class);
-            FileInputFormat.addInputPath(job, new Path(args[0]));
-            FileOutputFormat.setOutputPath(job, new Path(args[1]));
-            job.waitForCompletion(true);
-            iter++;
+
+            //create an arraylist with the result to return
+            try{
+              Path pt = new Path(KVALUES+"/part-r-00000");
+              BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(pt)));
+              String line;
+              line=br.readLine();
+              while (line != null){
+                result.add(Double.parseDouble(line));
+                line=br.readLine();
+              }
+            }catch(Exception e){
+              //doing nothing for now
+            }
+
+
+
           }
-
-
         }
       }
-// 1649.1670905672895	
-// 3977.2980141843973	
-// 6325.872458410351	
-// 8655.828378913991	
-// 11474.029603919967	
-// 15693.360868414085	
-// 58679.348840165236	
-// 394253.94670846395	
-// 692453.8689320388	
-// 2377288.5634674923	
+      return result;
+  }
 
-    }
 
   public static void getValues(Configuration conf , Path file , int k, int col){
     try{
